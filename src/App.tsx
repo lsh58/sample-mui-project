c6278ef3a4a0b93f310192560464386cbae28ab5
lsh58 <tmdgusdl58@gmail.com>
2020-11-07 18:57:49 +0900

    [docs] mui 설치 및 기본설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { CssBaseline, Container, Box } from '@material-ui/core';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container fixed>
+        <Box
+          component="div"
+          style={{ backgroundColor: '#cfe8fc', height: '100vh' }}
+        />
+      </Container>
+    </React.Fragment>
+  );
 }
 
 export default App;
