1849165c222e37e6e4e3faf89494ab8bf736566d
lsh58 <tmdgusdl58@gmail.com>
2021-02-01 22:36:02 +0900

    기본 베이스 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { CssBaseline, Container, Box } from '@material-ui/core';
+import React from "react";
+import { Box } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+    height: "100vh",
+    background: theme.palette.grey[900],
+  },
+}));
 
 function App() {
-  return (
-    <React.Fragment>
-      <CssBaseline />
-      <Container fixed>
-        <Box
-          component="div"
-          style={{ backgroundColor: '#cfe8fc', height: '100vh' }}
-        />
-      </Container>
-    </React.Fragment>
-  );
+  const classes = useStyles();
+  return <Box className={classes.root}></Box>;
 }
 
 export default App;
